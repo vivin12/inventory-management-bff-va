4ec9e106ceafa597e2f0911cc67cff4aef73bc60
VIVAN ABRAHAM <vivanabraham@VIVANs-MacBook-Pro.local>
2019-12-19 17:47:17 -0600

    Adds a mock service implementation

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from './health.controller';
+export * from './stock-items.controller';
 
---
+++ src/controllers/stock-items.controller.ts
@@ -0,0 +1,8 @@
+import {GET, Path} from 'typescript-rest';
+@Path('stock-items')
+export class StockItemsController {
+@GET
+async listStockItems(): Promise<any[]> {
+    return [];
+  }
+}
